57bd4a26f376c26d294837eed62e6c323f93b33b
Tom Reznik <tom@contentful.com>
2015-01-07 13:34:35 +0100

    Assert `Entry` subclass with no content type fails

--- test/test_resources.py
+++ test/test_resources.py
@@ -3,6 +3,17 @@ from test import BaseTestCase
 
 
 class ResourcesTestCase(BaseTestCase):
+    def test_fails_entry_no_contenttype(self):
+        exception = None
+        try:
+            class BadEntry(Entry):
+                pass
+        except AttributeError as e:
+            exception = e
+
+        self.assertIsNotNone(exception)
+        self.assertEqual('Class BadEntry does not have a __content_type__ specified.', exception.message)
+
     def test_repr(self):
         for clz in [Asset, ContentType, Entry, Space]:
             self.assertEqual(clz({'id': 'id'}).__repr__(), '<{0}(sys.id=id)>'.format(clz.__name__))
