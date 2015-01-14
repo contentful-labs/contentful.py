a92ce417ea4f89b270955ea6c3730ceafad6437f
Tom Reznik <tom@contentful.com>
2015-01-14 18:16:28 +0100

    Documentation at Wed Jan 14 18:16:28 CET 2015

--- contentful/__init__.py
+++
@@ -1,3 +0,0 @@
-from .cda.client import Client
-
-__version__ = '0.9.0a1'
--- contentful/cda/const.py
+++
@@ -1,7 +0,0 @@
-"""Constants module."""
-
-CDA_ADDRESS = 'cdn.contentful.com'
-
-PATH_ASSETS = 'assets'
-PATH_ENTRIES = 'entries'
-PATH_CONTENT_TYPES = 'content_types'
